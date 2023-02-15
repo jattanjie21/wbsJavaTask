2a6e1552d474bdd5c585cad39b31821e45737c86
jattanjie21 <laboyjatta@gmail.com>
2023-02-15 02:58:24 +0000

    Updated bug

--- root/src/test/java/util/StudentsTest.java
+++ root/src/test/java/util/StudentsTest.java
@@ -1,13 +1,9 @@
 package util;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import model.*;
-
-import static java.util.stream.Collectors.*;
-import static java.util.Comparator.*;
-import static java.util.Collections.*;
-import static java.util.Arrays.*;
 
 public class Students {
 
@@ -15,12 +11,12 @@ public class Students {
     private final Collection<Student> students;
 
     // Private constructor, used by the factory method
-    private Students(Student... students) {
-        this.students = unmodifiableCollection(asList(students));
+    private Students(Student students) {
+        this.students = List.of(students);
     }
 
     // Public factory method
-    public static Students students(Student... students) {
+    public static Students students(Student students) {
         return new Students(students);
     }
 
@@ -28,23 +24,25 @@ public class Students {
     // given years (inclusive of from and to)
     public Collection<Student> startingBetween(int from, int to) {
         return students.stream()
-            .filter(s -> s.getStart().getYear() >= from && s.getStart().getYear() <= to)
-            .collect(toList());
+                .filter(s -> s.getStartYear().getValue() >= from && s.getStartYear().getValue() <= to)
+                .collect(Collectors.toList());
     }
 
     // Return all students that finish(ed) between the the two given
     // years (inclusive of from and to).
     public Collection<Student> finishingBetween(int from, int to) {
         return students.stream()
-            .filter(s -> s.getFinish() != null && s.getFinish().getYear() >= from && s.getFinish().getYear() <= to)
-            .collect(toList());
+                .filter(s -> s.getFinishYear().isPresent()
+                        && s.getFinishYear().get().getValue() >= from
+                        && s.getFinishYear().get().getValue() <= to)
+                .collect(Collectors.toList());
     }
 
     // Return all students grouped by start year.
     // The result should be a Map from school year to a collection of students.
     public Map<SchoolYear, ? extends Collection<Student>> groupedByStartYear() {
         return students.stream()
-            .collect(groupingBy(s -> s.getStart().getYear()));
+                .collect(Collectors.groupingBy(Student::getStartYear));
     }
 
     // For the given school year, return counts of the number of students for
@@ -52,8 +50,8 @@ public class Students {
     // The result should be a Map from course length to a count of students.
     public Map<Integer, Long> countOfCourseLengthforStartYear(SchoolYear startYear) {
         return students.stream()
-            .filter(s -> s.getStart().equals(startYear))
-            .collect(groupingBy(s -> s.getCourse().getLength(), counting()));
+                .filter(s -> s.getStartYear().equals(startYear))
+                .collect(Collectors.groupingBy(Student::getCourseLength, Collectors.counting()));
     }
 
     // For the given school year, return counts of the number of students for
@@ -61,16 +59,23 @@ public class Students {
     // The result should be a Map from country code to a count of students.
     public Map<String, Long> countOfCountryCodeForStartYear(SchoolYear startYear) {
         return students.stream()
-            .filter(s -> s.getStart().equals(startYear))
-            .collect(groupingBy(s -> s.getCountryCode(), counting()));
+                .filter(s -> s.getStartYear().equals(startYear))
+                .collect(Collectors.groupingBy(Student::getCountryCode, Collectors.counting()));
     }
 
     // Return a list of students sorted in ascending order by id
     public List<Student> orderedById() {
         return students.stream()
-            .sorted(comparing(Student::getId))
-            .collect(toList());
+                .sorted(Comparator.comparing(Student::getId))
+                .collect(Collectors.toList());
     }
 
     // Return a list of students sorted in ascending order by name,
-    // if two students have the same
+    // if two students have the same name, they should be sorted by id
+    public List<Student> orderedByNameThenId() {
+        return students.stream()
+                .sorted(Comparator.comparing(Student::getName).thenComparing(Student::getId))
+                .collect(Collectors.toList());
+    }
+
+}
